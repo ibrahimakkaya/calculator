560bd45134a2755903a497a8dfd419af2abf57d2
ibrahim <halil.ibrahim.akkaya@icloud.com>
2022-07-04 19:48:42 +0300

    Connect math operator buttons with logic

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const buttons = document.querySelectorAll(".btn");
-// console.log(buttons);
 
 const screenWriting = document.querySelector(".writing");
 screenWriting.textContent = "";
@@ -10,21 +9,29 @@ const screenPocket = document.querySelector(".pocket");
 screenPocket.textContent = "";
 
 let calcArray = [];
+let calcArrayLastItem;
 
 buttons.forEach((button) => {
   button.addEventListener("click", clickEvent);
 });
 
 function clickEvent() {
+  if (this.textContent == "CLEAR") {
+    calcArray = [];
+    screenPocket.textContent = "";
+    screenWriting.textContent = "";
+  }
   if (+this.textContent >= 0 && +this.textContent <= 9) {
     writeToWritingScreen(this.textContent);
-  } else {
-    if (!(this.textContent == "=")) {
-      screenPocket.textContent += screenWriting.textContent + this.textContent;
-      screenWriting.textContent = "";
-    } else {
-      calculate();
+  } else if (calcArray.length < 3 && screenWriting.textContent != "") {
+    calcArray.push(screenWriting.textContent);
+    calcArray.push(this.textContent);
+
+    if (calcArray.length == 4) {
+      calculateArray(calcArray);
     }
+    screenPocket.textContent = calcArray[0] + calcArray[1];
+    screenWriting.textContent = "";
   }
 }
 
@@ -49,8 +56,9 @@ function calculateArray(array) {
       result = Number(array[0]) / Number(array[2]);
       break;
   }
-  console.log(result);
+  array[0] = result;
+  array[1] = array[3];
+  array.pop();
+  array.pop();
+  return array[0];
 }
-
-let array = ["200", "x", "2"];
-calculateArray(array);
